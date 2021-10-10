c892b89e508927c6c1183f59057c5c387e386b88
Konstantin2014 <kokos201475@gmail.com>
2021-10-10 19:25:14 +0300

    gallery

---
+++ src/js/marconi.js
@@ -0,0 +1,19 @@
+// var $grid = $('.gallery').masonry({
+//   itemSelector: '.gallery__item',
+//   columnWidth:'.grid_columnWidth' ,
+//     gutter: '.gutter_sizer'
+//   });
+
+//   $grid.on( 'click', '.gallery__item', function() {
+//     // change size of item via class
+//     $( this ).toggleClass('grid__item_gigante');
+//     // trigger layout
+//     $grid.masonry();
+//   });
+  
+
+// // layout Masonry after each image loads
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry();
+// });
+
